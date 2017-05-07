9f1af90fcf901a013c47479a821cf7a41a3ac7bd
Joan Gamell <gamell@gmail.com>
2017-05-07 08:43:04 -0700

    Uploading to S3, diffing changes with previous json

--- index.js
+++ index.js
@@ -3,9 +3,27 @@ const ghPinnedRepos = require('./gh-pinned-repos.js');
 const rssParser = require('rss-parser');
 const striptags = require('striptags');
 const truncate = require('truncate-html');
+const gzip = require('gzip-js');
+const pako = require('pako');
+const _ = require('lodash');
 
+// AWS stuff
+
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+
+// environment
+
+const env = process.env.ENVIRONMENT || 'local';
+
+const jsonUrl = 'https://s3.amazonaws.com/gamell-io/data.json';
 const pictureUrl = `https://api.500px.com/v1/photos?feature=user&username=gamell&sort=created_at&image_size=4&consumer_key=cdpKv8cJK8u78zzqd8WdUUlRGx1In8k0pDrviX62`;
 const articlesUrl = `https://medium.com/feed/@gamell`
+
+if(env !== 'prod'){ // load credentials if in development
+  const awsCredentials = new aws.SharedIniFileCredentials({profile: 'default'});
+  aws.config.credentials = awsCredentials;
+}
 
 function trimPictureInfo(data){
   return data.photos.map(i => {
@@ -14,7 +32,6 @@ function trimPictureInfo(data){
 }
 
 function parseFeed(feed){
-  debugger;
   return new Promise((resolve, reject) => {
     rssParser.parseString(feed, (error, data) => {
       if(error) reject(error);
@@ -25,7 +42,7 @@ function parseFeed(feed){
 
 function trimFeed(data){
   return data.feed.entries.map(e => {
-    let content = striptags(e['content:encoded'], ['b', 'strong', 'i', 'em', 'img'], ' ');
+    let content = striptags(e['content:encoded'], ['b', 'strong', 'i', 'em', 'img'], '');
     content = truncate(content, 50, {byWords: true});
     return {
       link: e.link,
@@ -34,28 +51,55 @@ function trimFeed(data){
       categories: e.categories,
       content: content
     }
+  });
+}
+
+function uploadToS3(data){
+  return new Promise((resolve, reject) => {
+    s3.putObject({
+        Bucket: 'gamell-io',
+        Key: 'data.json',
+        Body: data,
+        ContentType: 'application/json; charset=utf-8',
+        CacheControl: 'max-age=86400',
+        ContentEncoding: 'gzip',
+        ACL: 'public-read'
+    }, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
   })
-  return feed;
 }
 
 exports.handler = (event, context, callback) => {
+    const old = axios.get(jsonUrl).then(res => res.data).catch(err => new Object());
     const pictures = axios.get(pictureUrl).then(res => trimPictureInfo(res.data));
     const articles = axios.get(articlesUrl).then(res => parseFeed(res.data)).then(data => trimFeed(data));
     const repos = ghPinnedRepos.get('gamell');
 
-    Promise.all([pictures, articles, repos]).then(data => {
-      const output = {
+    Promise.all([pictures, articles, repos, old]).then(res => {
+      const [pictures, articles, repos, old] = res;
+      const data = { pictures, articles, repos };
+      if (!old.data) {
+        console.log('WARNING: Old data not available');
+        old.data = { pictures: [], articles: [], repos: [] };
+      }
+      if (JSON.stringify(old.data) === JSON.stringify(data)) {
+        // we do nothing if there are no updates
+        callback(null, 'success - no changes');
+        return;
+      }
+      const json = JSON.stringify({
         name: '@gamell',
         contents: '500px user feed, Medium articles, Github pinned repos',
-        //pictures: data[0],
-        articles: data[1],
-        //repos: data[2]
-      }
-      console.log(JSON.stringify(output));
-      callback(output, 'done');
+        timestamp: new Date(),
+        data
+      }, null, 2);
+      const gzipped = Buffer.from(pako.gzip(json), 'utf-8');
+      return uploadToS3(gzipped).then(callback(null, 'success - changes uploaded to S3'));
     }).catch(reason => {
       console.log(`ERROR!!! ${reason}`);
-      callback(reason, 'error');
+      callback(`ERROR!!! ${reason}`);
     })
 
 };
--- run.js
+++ run.js
@@ -1,3 +1,5 @@
 const app = require('./index.js');
 
-app.handler('bla','bla',function(){});
+app.handler('bla','bla',function(err, success){
+  console.log(success);
+});
